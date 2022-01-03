982e463ac3bca9efd7e466900f7aff8e361fd828
nikcoder01 <nikhil896160@gmail.com>
2022-01-04 01:35:27 +0530

    change in hello world program

--- helloworld.java
+++ helloworld.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class helloworld {
         public static void main(String[] args) {
           Scanner sc=new Scanner(System.in);
-          System.out.println("Hello World lololo");
+          System.out.println("Hello World lolololo");
           int x;
           x=sc.nextInt();
           System.out.println("The number is = "+x);
